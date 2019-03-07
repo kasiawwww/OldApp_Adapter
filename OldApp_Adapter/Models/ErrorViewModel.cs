174af5b24aef035c8e987055a12013d008e46bbe
test <k.widlak@microsoft.wsei.edu.pl>
2019-03-07 19:24:50 +0100

    Add project files.

---
+++ OldApp_Adapter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OldApp_Adapter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
